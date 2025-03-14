fa61b744740530f3aa8c8686693c0d347b304e19
pedroalma <pedroalma554@gmail.com>
2025-03-14 12:00:10 -0300

    novafinal

--- js.js
+++ js.js
@@ -14,3 +14,10 @@ atualizarVariaveisCSS();
 
 // Atualiza automaticamente sempre que a janela for redimensionada
 window.addEventListener('resize', atualizarVariaveisCSS);
+function mudarTexto() {
+    document.getElementById("meuTexto").innerHTML = "O texto foi alterado!";
+}
+
+function restaurarTexto() {
+    document.getElementById("meuTexto").innerHTML = "Este é o texto original.";
+}
