d60b68230e77a266f0c48c99474358fa23f878e4
pedroalma <pedroalma554@gmail.com>
2025-03-17 11:49:30 -0300

    hv

--- javinha.js
+++ javinha.js
@@ -1,5 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const div = document.getElementById("caixatpd");
-    const dimensoes = div.getBoundingClientRect();
-    console.log(`Largura: ${dimensoes.width}px; Altura: ${dimensoes.height}px;`);
-});
+// document.addEventListener("DOMContentLoaded", () => {
+//     const div = document.getElementsByClassName("caixatpd");
+//     console.log(`Largura: ${div.width}px; Altura: ${div.height}px;`);
+// });
+// function atualizar() {
+//     const argura = window.innerWidth("caixatpd");
+//     const ltura = window.innerHeight("caixatpd");
+
+//     document.documentElement.style.cssText = `
+//         --argura: ${argura}px;
+//         --ltura: ${ltura}px;
+//     `;
+// }
+
+// // Chama a função ao carregar a página
+// console.log(`--argura: ${argura}px;`);
+const div = document.getElementById(("caixatpd"));
+
+if (div) {
+    const largura = div.offsetWidth;
+    const altura = div.offsetHeight;
+
+    console.log(`Largura: ${largura}px, Altura: ${altura}px`);
+} else {
+    console.error("Elemento não encontrado!");
+}
